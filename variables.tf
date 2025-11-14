ba60619e9f0fd61d6cdd8a5bbab5370b51d66b80
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-11-14 15:59:21 +0530

    terraform commit

--- variables.tf
+++ variables.tf
@@ -48,3 +48,33 @@ variable "database_subnet_tags" {
     type = map
     default = {}
 }
+
+variable "public_route_table_tags" {
+    type = map
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    type = map
+    default = {}
+}
+
+variable "database_route_table_tags" {
+    type = map
+    default = {}
+}
+
+variable "eip_tags" {
+    type = map
+    default = {}
+}
+
+variable "nat_gateway_tags" {
+    type = map
+    default = {}
+}
+
+variable "is_peering_required" {
+    type = bool
+    default = true
+}
